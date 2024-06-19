343b150200e804dbd25e7d88261b83900433b8a3
sofiaizabella <sitc@academy.cin.ufpe.br>
2024-06-19 11:10:47 -0300

    assets

---
+++ lola/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  lola
+//
+//  Created by Sofia Izabella Tenório Chaves on 19/06/24.
+//
+
+import SwiftUI
+
+struct TelaInicialView: View {
+    var body: some View {
+        ZStack{
+            Color.rosaClaro
+            HStack {
+            }
+        }
+        .ignoresSafeArea()
+    }
+}
+
+#Preview {
+    TelaInicialView()
+}
---
+++ lola/lolaApp.swift
@@ -0,0 +1,17 @@
+//
+//  lolaApp.swift
+//  lola
+//
+//  Created by Sofia Izabella Tenório Chaves on 19/06/24.
+//
+
+import SwiftUI
+
+@main
+struct lolaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            TelaInicialView()
+        }
+    }
+}
